6bc5b661fd37082a611bd7fd263ee149bab73d08
Willem-Jaap <willemjaapk@gmail.com>
2023-01-25 22:34:49 +0100

    Fix projects -> devices notation.

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,23 +26,17 @@ const config: PlaywrightTestConfig = {
     projects: [
         {
             name: 'chromium',
-            use: {
-                ...devices['Desktop Chrome'],
-            },
+            use: devices['Desktop Chrome'],
         },
 
         {
             name: 'firefox',
-            use: {
-                ...devices['Desktop Firefox'],
-            },
+            use: devices['Desktop Firefox'],
         },
 
         {
             name: 'webkit',
-            use: {
-                ...devices['Desktop Safari'],
-            },
+            use: devices['Desktop Safari'],
         },
     ],
 
