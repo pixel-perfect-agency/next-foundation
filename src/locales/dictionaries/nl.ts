de21580e5fa3e0cf19f36d9d4abf1c85951c6432
Willem-Jaap <willemjaapk@gmail.com>
2023-10-13 15:38:41 +0200

    style: fix typo in dutch translation

--- src/locales/dictionaries/nl.ts
+++ src/locales/dictionaries/nl.ts
@@ -1,7 +1,7 @@
 const dictionary = {
     'home.title': 'Next Foundation',
     'home.description': 'Een solide blueprint voor het bouwen van schaalbare Next.js projecten.',
-    'component.location': 'Dit component staat in',
+    'component.location': 'Deze component staat in',
     'change-locale': 'Verander taal',
 } as const;
 
