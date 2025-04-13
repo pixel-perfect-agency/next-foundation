3cdb39d3b1c7f8196ca96627d0fb387dd5cb2f49
Willem-Jaap <willemjaapk@gmail.com>
2025-04-13 12:57:51 +0200

    style: format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import Component from '~components/component';
 const Page = async () => {
     return (
         <div className="m-8">
-            <h1 className="font-semibold text-2xl">
-                Next Foundation
-            </h1>
+            <h1 className="font-semibold text-2xl">Next Foundation</h1>
             <Component />
         </div>
     );
