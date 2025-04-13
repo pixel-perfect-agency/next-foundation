1fbe70f09be86f71715a4e76740b584b913ae030
Willem-Jaap <willemjaapk@gmail.com>
2025-04-13 11:35:36 +0200

    fix: cleanup localized code

--- src/components/misc/component.tsx
+++ src/components/misc/component.tsx
@@ -1,45 +1,16 @@
 'use client';
 
-import { useChangeLocale, useCurrentLocale, useT } from '~locales/client';
-import cn from '~utils/cn';
-
 const Component = () => {
-    const t = useT();
-    const changeLocale = useChangeLocale();
-    const currentLocale = useCurrentLocale();
-
     return (
         <div className="my-4 w-fit rounded-lg border border-gray-200 p-4">
             <h2 className="font-semibold text-xl">Hello World!</h2>
             <p>
-                {t('component.location')}{' '}
+                This component is located is located in:{' '}
                 <span className="rounded-md bg-gray-200 px-2 py-1 text-sm">
                     src/components/misc/component.tsx
                 </span>
             </p>
-            <div className="mt-4 flex gap-4">
-                {t('change-locale')}:
-                <div className="flex gap-2">
-                    <button
-                        type="button"
-                        className={cn('rounded-md bg-gray-200 px-2 py-1 text-sm', {
-                            'bg-blue-500 text-white': currentLocale === 'en',
-                        })}
-                        onClick={() => changeLocale('en')}
-                    >
-                        en
-                    </button>
-                    <button
-                        type="button"
-                        className={cn('rounded-md bg-gray-200 px-2 py-1 text-sm', {
-                            'bg-blue-500 text-white': currentLocale === 'nl',
-                        })}
-                        onClick={() => changeLocale('nl')}
-                    >
-                        nl
-                    </button>
-                </div>
-            </div>
+           
         </div>
     );
 };
